8e0ef324e90d7f7635c5e60617ee150af6646b17
PINTFORGE <29993075+pintforge@users.noreply.github.com>
2020-05-28 22:55:28 +0100

    Add files via upload

---
+++ l10n_ma_hr_payroll/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Morocco Payroll',
+    'category': 'Human  Resources',
+    'depends': [
+    "hr_payroll",
+    ],
+    'description': """
+Moroccan Payroll Salary Rules.
+==============================
+-Configuration of hr_payroll for Morocco localization
+    -All main contributions rules for Morocco payslip.
+
+    """,
+    'data': [
+        'views/l10n_ma_hr_payroll_view.xml',
+        'data/l10n_ma_hr_payroll_data.xml',
+     ],
+    'depends': ['hr'],
+}
